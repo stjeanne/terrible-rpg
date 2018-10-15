df676dea02dcc7f6a2f2975b9e80bb7279da6799
jeanne <jwthornton@gmail.com>
2018-10-14 20:13:25 -0400

    commands now change HTML

--- commands.js
+++ commands.js
@@ -1,8 +1,16 @@
 // commands for capitalism zero
 
-var statWindow = document.getElementById("statwindow");
-
+var commandLog;
 
 var cmd_idle = function() {
 	console.log("clicked idle");
+	commandLog = document.getElementById("statwindow");
+//	var currentLog = commandLog.innerHTML;
+	var currentLog = "";
+	commandLog.innerHTML = currentLog + "<p>* Idling!</p>";
+}
+
+var cmd_clean = function() {
+	console.log("clicked clean");
+	commandLog.innerHTML = "<p>* You clean the area.</p>";
 }
